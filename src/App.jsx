69a490559a624022eb234092d22a06c4f576bcbb
David Emmanuel <eclefzy@gmail.com>
2025-10-20 11:12:21 +0100

    worked on the NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Router, Routes, Route} from 'react-router-dom';
 import './App.css'
 import Dashboard from './components/Dashboard';
-import ModulePage from "./components/ModulePage";
+// import ModulePage from "./components/ModulePage";
 import NotFound from './components/NotFound';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
 
         {/* Dynamic module page */}
-        <Route path="/modules/:id" element={<ModulePage />} />
+        {/* <Route path="/modules/:id" element={<ModulePage />} /> */}
       </Routes>
     </>
   )
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div>
       <h2>Oops! Page not found</h2>
-      {/* <button className='btn btn-primary' to='/'>Home</button> */}
+      <Link to='/' className='btn btn-primary'>Go to Dashboard</Link>
     </div>
   )
 }
