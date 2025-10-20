3e579303ce52042c927b61d09f5657f4040d111f
David Emmanuel <eclefzy@gmail.com>
2025-10-20 17:14:23 +0100

    moved the dashboard to the dashboard folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Router, Routes, Route} from 'react-router-dom';
 import './App.css'
-import Dashboard from './components/Dashboard';
+import Dashboard from './dashboard/Dashboard';
 import ModulePage from "./components/ModulePage";
 import NotFound from './components/NotFound';
 
